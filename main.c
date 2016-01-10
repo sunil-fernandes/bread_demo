8ef4520cd7bc3438a37bb170e30b63dbd69616f4
Sunil Fernandes <sunil.fernandes@microchip.com>
2016-01-10 17:07:02 +0530

    Team 1 added one more comment for release 2

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(void)
 {
     // This was added by team 1 for release 2
+    // team 1 Added one more feature for release 2
     printf("This was a change to the mainline");
     while(1);
 }
